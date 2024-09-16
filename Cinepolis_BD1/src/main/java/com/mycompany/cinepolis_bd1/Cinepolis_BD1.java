c96d9465f239f260fc720dbe10f1df32bf627d66
Lunatikera <carlitosjuven@gmail.com>
2024-09-15 20:04:30 -0700

    Inicio del Proyecto
    Creacion del Proyecto

---
+++ Cinepolis_BD1/src/main/java/com/mycompany/cinepolis_bd1/Cinepolis_BD1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinepolis_bd1;
+
+/**
+ *
+ * @author carli
+ */
+public class Cinepolis_BD1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Cinepolis_BD1/src/main/java/persistencia/ICrud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package persistencia;
+
+/**
+ *
+ * @author carli
+ */
+public interface ICrud <T> {
+//    public T guardar()
+//    public T editar
+//    public T eliminar
+//    public T leerTodos
+}
